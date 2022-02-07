7b3d8bab0f84035ed18f5fb0eabce103c391ddb1
Aleksei <Aleksei_Kanaev@epam.com>
2022-02-07 17:45:17 +0300

    Modified task1.2 for ram implementation

--- task2.js
+++ task2.js
@@ -1,10 +1,13 @@
 
+const USE_RAM = process.env.USE_RAM == 1 || false // use implementation to load files into RAM or STREAMS (stream by default)
+console.debug(`You're about to run ${USE_RAM ? 'full RAM' : 'stream'} implementation`)
+
 const csv2json = require('node-csvtojson'),
     { readdir, stat } = require('fs/promises'),
     { basename, extname, resolve } = require('path'),
     fs = require('fs');
 
-const pipeline = require('util').promisify(require('stream').pipeline);
+let pipeline;
 
 
 const directory = 'csv';
@@ -20,19 +23,41 @@ const directory = 'csv';
 })()
 
 async function doFile(file) {
-    console.log('...File ' + file)
     try {
-        let fileStat = await stat(file)
-        if (fileStat?.isFile()) {
-            await processCsvOld(file,
-                resolve(directory, basename(file, extname(file)) + '.txt'))
+        const fileStat = await stat(file)
+        const file_extname = extname(file)
+        if (fileStat?.isFile() && file_extname === '.csv') {
+            console.log('...File ' + file)
+            await processCsv(file,
+                resolve(directory, basename(file, file_extname) + '.txt'))
         }
     } catch(ee) {
         console.error(`Failed to process file ${file}: ${ee}`)
     }
 }
 
-async function processCsvOld(inputPath, outputPath) {
+async function processCsvRAM(inputPath, outputPath) {
+    return new Promise((rslv, rjct) => {
+        const resp_json = []
+        csv2json()
+        .fromFile(inputPath)
+        .subscribe((json) => {
+            resp_json.push(JSON.stringify(json))
+        },
+        (error) => reject(error),
+        () => fs.writeFile(outputPath, resp_json.join('\r\n'), (err) => {
+            if (err) rjct(err)
+            else rslv()
+        }))
+    })
+    
+}
+
+async function processCsvStream(inputPath, outputPath) {
+    if (!pipeline) {
+        pipeline = require('util').promisify(require('stream').pipeline);
+    }
+
     // using node 14 lts
     await pipeline(
         fs.createReadStream(inputPath),
@@ -41,3 +66,4 @@ async function processCsvOld(inputPath, outputPath) {
     )
 }
 
+const processCsv = (USE_RAM)? processCsvRAM : processCsvStream;
