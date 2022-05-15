d627b3157ab5ccf5d25a583f78c859eea7c14d7b
Aleksei <Aleksei_Kanaev@epam.com>
2022-05-16 02:45:25 +0300

    Task 3.1

--- task2/task2.2.js
+++ task2/task2.2.js
@@ -3,6 +3,16 @@ const express = require('express');
 const joi = require('joi');
 const validator = require('express-joi-validation').createValidator({});
 
+const { Sequelize, DataTypes, Op } = require('sequelize');
+const sequelize = new Sequelize('postgres://ygyhynng:bz2Yyaksava6geCCIiM_GEe6L-lp10gc@lallah.db.elephantsql.com/ygyhynng');
+
+try {
+    sequelize.authenticate();
+    console.log('Connected to DB');
+} catch (e) {
+    console.log('Failed to connect to DB: ', e)
+}
+
 const app = express();
 app.use(express.json());
 
@@ -11,96 +21,110 @@ app.use(function(req, _res, next) {
     next();
 });
 
-class User {
-    constructor(id, login, password, age, isDeleted = false) {
-        this.id = id ?? get_uuid();
-        this.login = login;
-        this.password = password;
-        this.age = age;
-        this.isDeleted = isDeleted;
+const predefined_users = [
+    {login: 'user1', password: '123qwe', age: 10},
+    {login: 'user2', password: '456ert', age: 100},
+    {login: 'user3', password: 'asd657', age: 50}
+];
+
+const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true
+    },
+    login: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    age: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    isDeleted: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
     }
-}
+}, {
+    timestamps: false
+});
 
 class Users {
     constructor() {
-        this.users = [];
+        sequelize.sync({});
+        
+        User.bulkCreate(predefined_users);
     }
 
-    find(id) {
-        return id && this.users.find(u => u.id === id);
-    }
-
-    get(id) {
-        const user = this.find(id);
-        if (user && user.isDeleted !== true) {
-            const retUser = { ...user };
-            delete retUser.isDeleted;
-            return retUser;
-        }
-        return null;
-    }
-
-    create({id, login, password, age}) {
-        if (this.find(id)) {
-            throw new Error(`Cannot create - user id ${id} already exists`);
-        }
-
-        if (this.users.some(u => u.login === login)) {
-            throw new Error(`Cannot create - user with the same login ${login} already exists`);
-        }
-        const newUser = new User(id, login, password, age);
-        this.users.push(newUser);
-        return newUser.id;
-    }
-
-    update({id, login, password, age}) {
-        const user = this.find(id);
-        if (!user || user.isDeleted === true) {
-            throw new Error(`Cannot update - user id ${id} does not exists or removed`);
-        }
-
-        if (login) user.login = login;
-        if (password) user.password = password;
-        if (age) user.age = age;
-    }
-
-    delete(id, soft = true) {
-        const user = this.find(id);
-        if (!user) {
-            throw new Error(`Cannot remove - user id ${id} does not exists`);
-        }
-        if (soft) {
-            user.isDeleted = true;
-        } else {
-            this.users = this.users.filter(u => u.id !== id);
-        }
-    }
-
-    getAutoSuggestUsers(loginSubstring, limit = 0) {
-        const retUsers = this.getAll()
-            .filter(u => u.login && u.login.includes(loginSubstring))
-            .sort((u1, u2) => u1.login > u2.login ? 1 :
-                            u1.login < u2.login ? -1 : 0);
-        return limit > 0 ? retUsers.slice(0, limit) : retUsers;
-    }
-
-    getAll() {
-        console.log(this.users); // for debug 
-        return this.users.reduce((prev, curr) => {
-            if (curr.isDeleted !== true) {
-                const retUser = { ...curr };
-                delete retUser.isDeleted;
-                prev.push(retUser);
+    async get(id) {
+        return id && User.findAll({
+            attributes: {exclude: ['isDeleted']},
+            where: {
+                id,
+                isDeleted: {[Op.not]: true}
             }
-            return prev;
-        }, []);
+        });
+    }
+
+    async create({id, login, password, age}) {
+        const user = await User.create({ id, login, password, age });
+        return user.id;
+    }
+
+    async update({id, login, password, age}) {
+        await User.update({login, password, age}, {
+            where: {id}
+        });
+    }
+
+    async delete(id, soft = true) {
+        if (soft) {
+            await User.update({isDeleted: true}, {
+                where: {id}
+            });
+        } else {
+            await User.destroy({
+                where: {id}
+            });
+        }
+    }
+
+    async getAutoSuggestUsers(loginSubstring, limit) {
+        if (!(limit > 0)) { // validation for non-number
+            limit = undefined;
+        }
+        return User.findAll({
+            order: sequelize.col('login'),
+            limit,
+            attributes: {exclude: ['isDeleted']},
+            where: {
+                login: {
+                    [Op.like]: `%${loginSubstring}%`
+                },
+                isDeleted: {[Op.not]: true}
+            }
+        });
+    }
+
+    async getAll() {
+        return User.findAll({
+            attributes: {exclude: ['isDeleted']},
+            where: {
+                isDeleted: {[Op.not]: true}
+            }
+        });
     }
 }
 
 const users_db = new Users();
 
-app.get('/api/user', function(_req, res) {
-    res.json({users: users_db.getAll()});
+app.get('/api/user', async function(_req, res) {
+    res.json({users: await users_db.getAll()});
 });
 
 const querySchema = joi.object({
@@ -108,14 +132,14 @@ const querySchema = joi.object({
     login: joi.string().alphanum().required()
 });
 
-app.get('/api/user/auto-suggest', validator.query(querySchema), function(req, res) {
-    const users_limit = users_db.getAutoSuggestUsers(req.query.login, req.query.limit);
-    res.json({users: users_limit});
+app.get('/api/user/auto-suggest', validator.query(querySchema), async function(req, res) {
+    const users = await users_db.getAutoSuggestUsers(req.query.login, req.query.limit);
+    res.json({users});
 });
 
-app.get('/api/user/:id', function(req, res) {
+app.get('/api/user/:id', async function(req, res) {
     try {
-        const user = users_db.get(req.params.id);
+        const user = await users_db.get(req.params.id);
         if (user) {
             res.json({user});
         } else {
@@ -130,15 +154,15 @@ app.get('/api/user/:id', function(req, res) {
 const userSchema = joi.object({
   login: joi.string().alphanum().required(),
   password: joi.string()
-    .pattern(new RegExp('^.*(?=.{2,})(?=.*[0-9])(?=.*[a-zA-Z])[a-zA-Z0-9]+$'), 'letters and numbers').required(),
+    .pattern(/^.*(?=.{2,})(?=.*[0-9])(?=.*[a-zA-Z])[a-zA-Z0-9]+$/, 'letters and numbers').required(),
   age: joi.number().integer().min(4).max(130).required()
 });
 
-function handleCreateUser(req, res) {
+async function handleCreateUser(req, res) {
     try {
         const { login, password, age } = req.body;
-        const retId = users_db.create({ id: req.params.id, login, password, age});
-        res.json({status: "ok", userId: retId});
+        const userId = await users_db.create({ id: req.params.id, login, password, age});
+        res.json({status: "ok", userId});
     } catch(ee) {
         res.status(400).send(ee.toString());
     }
@@ -147,20 +171,20 @@ function handleCreateUser(req, res) {
 app.post('/api/user', validator.body(userSchema), handleCreateUser);
 app.post('/api/user/:id', validator.body(userSchema), handleCreateUser);
 
-app.put('/api/user/:id', validator.body(userSchema), function(req, res) {
+app.put('/api/user/:id', validator.body(userSchema), async function(req, res) {
     try {
         const { login, password, age } = req.body;
-        users_db.update({ id: req.params.id, login, password, age });
+        await users_db.update({ id: req.params.id, login, password, age });
         res.end('User updated');
     } catch(ee) {
         res.status(400).send(ee.toString());
     }
 });
 
-app.delete('/api/user/:id', function(req, res) {
+app.delete('/api/user/:id', async function(req, res) {
     try {
         const softRemove = req.query.soft === "false" ? false : true;
-        users_db.delete(req.params.id, softRemove);
+        await users_db.delete(req.params.id, softRemove);
         res.end('User deleted');
     } catch(ee) {
         res.status(400).send(ee.toString());
